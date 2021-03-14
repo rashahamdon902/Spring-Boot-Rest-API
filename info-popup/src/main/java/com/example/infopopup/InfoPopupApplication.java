2d57d908d385136b2b64525303fc215731f1998f
Fawaz Ahmad Kserawi <fawaz@qu.edu.qa>
2021-03-14 12:03:14 +0300

    Commiting 2nd test

---
+++ info-popup/src/main/java/com/example/infopopup/InfoPopupApplication.java
@@ -0,0 +1,13 @@
+package com.example.infopopup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPopupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoPopupApplication.class, args);
+	}
+
+}
